e828769828c13b05f4dcf3deef7948409aff0766
RafaelDavisH <thinkprintgrafx@gmail.com>
2018-06-21 21:04:01 -0700

    docs: Add README.md for the Password Generator
    Add reference to Code Club Projects and brief description of the password generator.

---
+++ Password Generator/README.md
@@ -0,0 +1,20 @@
+# Password Generator
+** This simple python project its from [Code Club Projects](https://codeclubprojects.org/en-GB/python/password-generator/)**
+
+> Simple program to generate random passwords using lowercase and uppercase letters, numbers and punctuations.
+The user will have the ability to choose the length and the number of passwords to be generated.
+
+``` python
+
+Password Generator
+==================
+
+password length? 10
+number of passwords? 3
+
+Here are your passwords:
+0$]iKpETmg
+he@PbV(L%Z
+2a(@IicKZW
+
+```
--- Password Generator/password_generator.py
+++ Password Generator/password_generator.py
@@ -3,22 +3,26 @@ import random
 
 # create a list of characters to choose from
 # containing letters, numbers and punctuation characters
-chars = 'abcdefghijklmnopqrstuvwxyzABCDEFHIJKLMNOPQRSTUVWXYZ123457890!#$@_+,?[]().-&*^%'
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFHIJKLMNOPQRSTUVWXYZ123457890!#$@_+,?[]().-&*^%"
 
 # print header
-header = 'Password Generator'
-print('\nPassword Generator')
-print('=' * len(header) + "\n")
+header = "Password Generator"
+print("\nPassword Generator")
+print("=" * len(header) + "\n")
+
 # Ask the user to input a password length, and store it in a variable
-length = input('password length? ')
+length = input("password length? ")
 length = int(length)  # turn the user's input into a whole number
 
 # Ask the user to input how many passwords want generated, and store it in a variable
-amount = input('number of passwords? ')
+amount = input("number of passwords? ")
 amount = int(amount)  # turn the user's input into a whole number
 
+# print passwords
+print('\nHere are your passwords:')
+
 for p in range(amount):
-    password = ''  # start with an empty variable
+    password = ""  # start with an empty variable
 
     for c in range(length):  # using the `length` variable to repeat as many times as the user entered.
         password += random.choice(chars)
