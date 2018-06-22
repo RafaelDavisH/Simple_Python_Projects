e6563fbdfa2851d5c1e1c7893b0ab778ebab7a0f
RafaelDavisH <thinkprintgrafx@gmail.com>
2018-06-21 20:17:26 -0700

    feat: Choosing a password length and number of passwords
    Allow user to choose the password lenght and number of passwords.

--- Password Generator/password_generator.py
+++ Password Generator/password_generator.py
@@ -3,10 +3,23 @@ import random
 
 # create a list of characters to choose from
 # containing letters, numbers and punctuation characters
-chars = 'abcdefghijklmnopqrstuvwxyzABCDEFHIJKLMNOPQRSTUVWXYZ123457890!#$@_+,?[]().-&*'
+chars = 'abcdefghijklmnopqrstuvwxyzABCDEFHIJKLMNOPQRSTUVWXYZ123457890!#$@_+,?[]().-&*^%'
 
-password = ''  # start with an empty variable
+# print header
+header = 'Password Generator'
+print('\nPassword Generator')
+print('=' * len(header) + "\n")
+# Ask the user to input a password length, and store it in a variable
+length = input('password length? ')
+length = int(length)  # turn the user's input into a whole number
 
-for c in range(10):
-    password += random.choice(chars)
-print(password)
+# Ask the user to input how many passwords want generated, and store it in a variable
+amount = input('number of passwords? ')
+amount = int(amount)  # turn the user's input into a whole number
+
+for p in range(amount):
+    password = ''  # start with an empty variable
+
+    for c in range(length):  # using the `length` variable to repeat as many times as the user entered.
+        password += random.choice(chars)
+    print(password)
